3e49a04b3210235a4485b503967adf5075235cd5
kev <kevbs.jsx@gmail.com>
2022-11-24 21:03:18 -0500

    creando form de login y registro

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import Link from 'next/link';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <form>
+        <h1>Register</h1>
+
+        <label>
+          Full Name
+          <input type='text' />          
+        </label>
+
+        <label>
+          Email
+          <input type='email' />          
+        </label>
+
+        <label>
+          Password
+          <input type='password' />
+        </label>
+
+        <label>
+          Confirm Password
+          <input type='password' />
+        </label>
+
+        <button type='submit'>Sign up</button>
+
+        <p>
+          Have an account ?
+          <Link href='/auth/register'>Click here</Link>
+        </p>
+
+      </form>
+    </>
   )
 }
 
-export default Register
+export default Register;
