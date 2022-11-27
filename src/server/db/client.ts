feead7a076933ee4a46ae723762352e9557104cb
kev <kevbs.jsx@gmail.com>
2022-11-27 15:57:04 -0500

    register-auth-complete

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-
+export * from '@prisma/client';
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,10 +1,40 @@
-import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { publicProcedure, router } from '../../trpc/trpc';
+import { prisma, Prisma, Role, Status } from '../../db/client';
+import { TRPCError } from '@trpc/server';
+import bcrypt from 'bcrypt';
+import { registerSchema } from '../../../common/auth';
+
+const userSelect = Prisma.validator<Prisma.UserSelect>()({
+  email: true, name: true, id: true, role: true, status: true,
+});
+
+export interface IUser {
+  email: string;
+  name: string;
+  id: string;
+  status: Status;
+  role: Role;
+}
 
 export const authRouter = router({
-  getSession: publicProcedure.query(({ ctx }) => {
-    return ctx.session;
-  }),
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
+    //? Register
+    register: publicProcedure
+    .input(registerSchema)
+    .mutation(async ({ input }) => {
+      const user = await prisma.user.findUnique({ where: { email: input.email } });
+      if (user) throw new TRPCError({ code: 'CONFLICT', message: 'Email address already in use' });
+      // create user
+      const createdUser = await prisma.user.create({
+        data: {
+          email: input.email.toLowerCase(),
+          password: bcrypt.hashSync(input.password, 10),
+          name: input.name,
+        },
+        select: userSelect,
+      });
+
+      if (!createdUser) throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'User not created ---SERVER_ERROR---' });
+
+      return createdUser;
+    }),
 });
