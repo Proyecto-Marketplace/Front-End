77d22fb65d323d8f1902ef33084cbd447909c63b
kev <kevbs.jsx@gmail.com>
2022-11-27 19:22:20 -0500

    backend para el auth terminado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,28 +1,85 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import Credentials from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
 import { prisma } from "../../../server/db/client";
+import { loginSchema } from "../../../common/auth";
+import bcrypt from 'bcrypt';
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID ?? '',
       clientSecret: process.env.DISCORD_CLIENT_SECRET ?? '',
     }),
-    // ...add more providers here
+    Credentials({
+      name: 'Credentials',
+      type: 'credentials',
+      credentials: { email: { type: 'email' }, password: { type: 'password' } },
+      async authorize(credentials) {
+        const creds = await loginSchema.spa(credentials);
+        if(!creds.success) throw Error(`${creds.error.issues[0]?.message}`);
+        const user = await prisma.user.findUnique({ where: { email: creds.data.email }});
+
+        // if(!user) return null;
+        if(!user) throw Error('User not found');
+        if(!bcrypt.compareSync(creds.data.password, user.password ?? '')) throw Error('Your password is incorrect');
+
+        const authed_user = await prisma.user.update({
+          where: { email: creds.data.email },
+          data: { status: 'online' },
+          select: { email: true, name: true, role: true, status: true, id: true }
+        });
+
+        return authed_user;
+      }
+    })
   ],
+  // Include user.id on session
+  callbacks: {
+    jwt: ({ token, user, isNewUser }) => {
+      // console.log({token, account, profile, user, isNewUser})
+      if(user) {
+        token.user = user;
+      };
+      if(isNewUser) {
+        token.isNewUser = isNewUser;
+      };
+      return token;
+    },
+    session({ session, user, token }) {
+      // console.log({ callbacks: { session, user, token } })
+      session.user = token.user
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/auth/login',
+    newUser: '/auth/register',
+  },  
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  // debug: true,
+  events: {
+    signIn: (msg) => {
+      console.log({signIn: msg})
+    },
+    signOut: (msg) => {
+      console.log({signOut: msg})
+    },
+    createUser: (msg) => {
+      console.log({ createUser: msg })
+    },
+    updateUser: (msg) => {
+      console.log({ updateUser: msg })
+    },
+    session: (msg) => {
+      console.log({ session: msg })
+    },
+  },
 };
 
 export default NextAuth(authOptions);
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,33 +1,58 @@
 import Link from 'next/link';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { loginSchema, LoginSchema as FormValues } from '../../common/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import { signIn, getProviders } from 'next-auth/react';
+import { LoginSchema as FormValues, loginSchema } from '../../common/auth';
+type CookieResponse = [string, string];
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
+import { getServerAuthSession } from '../../server/common/get-server-auth-session';
 
 const Login = () => {
 
-  const { register, formState: { errors }, handleSubmit } = useForm<FormValues>({
+  const router = useRouter();
+  const [ providers, setProviders ] = useState<any>({});
+  useEffect(() => {
+    getProviders().then(setProviders);
+  }, []);
+  const user: CookieResponse = JSON.parse(Cookies.get('data-form') || '[]');
+  const { register, handleSubmit, setError, formState: { errors, isSubmitSuccessful, isSubmitted, isSubmitting }, getValues } = useForm<FormValues>({
+    defaultValues: { email: user[0] || '', password: user[1] || '' },
     mode: 'all',
     resolver: zodResolver(loginSchema),
-    shouldFocusError: true
   });
 
-  const handleLogin: SubmitHandler<FormValues> = (data) => {
-    console.log({ data });
+  useEffect(() => {
+    router.prefetch(`${router.query.p}`)
+  }, [ router ]);
+  // const handleLogin: SubmitHandler<FormValues> = (data) => mutation.mutate(data);
+  const handleLogin: SubmitHandler<FormValues> = async (data) => {
+    const res = await signIn('credentials', { ...data, redirect: false });
+    if(res?.error) return setError('password', { type: 'wrong', message: res.error }, { shouldFocus: true });
+    if(res?.url?.includes('/auth/login')) return await router.push('/');
+    await router.push(res?.url ?? '/');
   }
 
   return (
     <div className='login'>
+      <span style={{ display: errors.email?.type === 'wrongE' || errors.password?.type === 'wrong' ? 'flex' : 'none' }}>
+        {errors.password?.message}
+      </span>
       <form className='login-form' onSubmit={handleSubmit(handleLogin)}>
         <h1 className='login-form-title'>Login</h1>
 
         <label className='login-form-label'>
           <span className='login-form-label-title'>Email</span>
           <input type='email' {...register('email')} />
+          <span>{errors.email?.type !== 'wrongE' && errors.email?.message}</span>
         </label>
 
         <label className='login-form-label'>
           <span className='login-form-label-title'>Password</span>
           <input type='password' {...register('password')} />
+          <span>{errors.password?.type !== 'wrong' && errors.password?.message}</span>
         </label>
 
         <button className='login-submit btn-submit' type='submit'>Sign in</button>
@@ -40,6 +65,13 @@ const Login = () => {
       </form>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const session = await getServerAuthSession(ctx);
+  if(session) return { redirect: { destination: ctx.req.headers.referer ?? '/', permanent: false } };
+
+  return { props: {} }
 }
 
 export default Login
