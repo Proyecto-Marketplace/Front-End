635c1db4765f57e78de4296c183d675c8abadd67
kev <kevbs.jsx@gmail.com>
2022-12-01 03:00:30 -0500

    fixed

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 export * from '@prisma/client';
+
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
